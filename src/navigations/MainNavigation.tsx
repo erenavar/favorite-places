a616a7266373f15dde794ae105df563a6a060e2d
Eren AVAR <eren.avar@hotmail.com>
2024-06-17 13:54:19 +0300

    adjust navigation settings

---
+++ src/navigations/MainNavigation.tsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FavouritesList from '../screens/FavouritesList';
+import MapScreen from '../screens/MapScreen';
+
+const Stack = createNativeStackNavigator();
+
+const MainNavigation = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name='Favourites' component={FavouritesList} />
+                <Stack.Screen name='Map' component={MapScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default MainNavigation
+
