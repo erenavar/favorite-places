fa70fc7cbba01b69020c8af53c5e33264646ccc3
Eren AVAR <eren.avar@hotmail.com>
2024-06-27 17:07:19 +0300

    create reducer and its types

--- src/navigations/MainNavigation.tsx
+++ src/navigations/MainNavigation.tsx
@@ -4,17 +4,27 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FavouritesList from '../screens/FavouritesList';
 import MapScreen from '../screens/MapScreen';
+import { Button } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 
 
-
 const MainNavigation = () => {
+
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator
+            >
                 <Stack.Screen name='Favourites' component={FavouritesList} />
-                <Stack.Screen name='Map' component={MapScreen} />
+                <Stack.Screen
+                    name='Map'
+                    component={MapScreen}
+                    options={{
+                        headerBackTitleVisible: false
+                    }}
+
+                />
             </Stack.Navigator>
         </NavigationContainer>
     )
--- src/redux/markerReducer.ts
+++ src/redux/markerReducer.ts
@@ -1,7 +1,33 @@
-const initialState = {
-    lat: "",
-    long: "",
-    title: "",
-    desc: "",
-    pinColor: ""
+import { PayloadAction, createSlice } from "@reduxjs/toolkit"
+
+
+export interface IMarkerState {
+    lat: number;
+    long: number;
+    title: string;
+    desc: string;
+    pinColor: string;
 }
+
+interface IMarkers {
+    markers: IMarkerState[]
+}
+
+
+const initialState: IMarkers = {
+    markers: []
+
+}
+
+const markerSlice = createSlice({
+    name: "markerSlice",
+    reducers: {
+        addMarker: (state, action: PayloadAction<IMarkerState>) => {
+            state.markers.push(action.payload);
+        }
+    },
+    initialState,
+})
+
+export const { addMarker } = markerSlice.actions;
+export default markerSlice.reducer;
