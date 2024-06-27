80c88bc092229b494ed581237cb94cd659219167
Eren AVAR <eren.avar@hotmail.com>
2024-06-27 18:12:31 +0300

    add expo crypto and take datas from redux to show on favourite list screen

--- src/screens/FavouritesList.tsx
+++ src/screens/FavouritesList.tsx
@@ -3,16 +3,19 @@ import React from 'react'
 import Button from '../components/Button'
 import { useNavigation } from '@react-navigation/native'
 import ListItem from '../components/ListItem';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
 
 
 
 export default function FavouritesList() {
 
+    const globalState = useSelector((state: RootState) => state.marker);
     const navigation = useNavigation();
     const toMapPage = () => {
         navigation.navigate('Map')
     }
-
+    console.log('globalState :>> ', globalState);
     return (
         <SafeAreaView style={{ flex: 1, marginTop: 15 }}>
             <ScrollView style={styles.scroll}>
